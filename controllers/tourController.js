8f71aeb32619b884ee135477ab106854fe1489ec
Zemichael Gebremeskel <zemae1015@gmail.com>
2019-09-28 08:51:35 +0200

    made aggregation pipline data and virtual properties

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,5 @@
 const Tour = require("../models/tourModel");
-const APIFeatures=require('../utils/apiFeatures');
+const APIFeatures = require("../utils/apiFeatures");
 
 // exports.checkID = (req, res, next, val) => {
 //   console.log(`Tour id is : ${val}`);
@@ -143,6 +143,94 @@ exports.deleteTour = async (req, res) => {
     return res.status(204).json({
       status: "success",
       data: null
+    });
+  } catch (err) {
+    return res.status(404).json({
+      status: "fail",
+      message: err
+    });
+  }
+};
+//Aggregate Data:
+exports.getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } }
+      },
+      {
+        $group: {
+          _id: { $toUpper: "$difficulty" },
+          numTours: { $sum: 1 },
+          numRatings: { $sum: "$ratingsQuantity" },
+          avgRating: { $avg: "$ratingsAverage" },
+          avgPrice: { $avg: "$price" },
+          minPrice: { $min: "$price" },
+          maxPrice: { $max: "$price" }
+        }
+      },
+      {
+        $sort: { avgPrice: 1 }
+      },
+      {
+        $match: { _id: { $ne: "EASY" } }
+      }
+    ]);
+    return res.status(200).json({
+      status: "success",
+      data: {
+        stats
+      }
+    });
+  } catch (err) {
+    return res.status(404).json({
+      status: "fail",
+      message: err
+    });
+  }
+};
+exports.getMonthlyPlan = async (req, res) => {
+  try {
+    const year = req.params.year * 1;
+    const plan = await Tour.aggregate([
+      {
+        $unwind: "$startDates"
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`)
+          }
+        }
+      },
+      {
+        $group: {
+          _id: { $month: "$startDates" },
+          numTourStarts: { $sum: 1 },
+          tours:{$push:'$name'}
+        }
+      },
+      {
+        $addFields:{month:'$_id'}
+      },
+      {
+        $project:{
+          _id:0
+        }
+      },
+      {
+        $sort:{numTourStarts:-1}
+      },
+      {
+        $limit:12
+      }
+    ]);
+    return res.status(200).json({
+      status: "success",
+      data: {
+        plan
+      }
     });
   } catch (err) {
     return res.status(404).json({
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,7 +3,11 @@ const tourController = require("../controllers/tourController");
 const router = express.Router();
 //Param middleware:
 //router.param('id', tourController.checkID);
-router.route("/top-5-cheap").get(tourController.aliasTopTours, tourController.getAllTours);
+router.route("/tour-stats").get(tourController.getTourStats);
+router.route("/monthly-plan/:year").get(tourController.getMonthlyPlan);
+router
+  .route("/top-5-cheap")
+  .get(tourController.aliasTopTours, tourController.getAllTours);
 router
   .route("/")
   .get(tourController.getAllTours)
